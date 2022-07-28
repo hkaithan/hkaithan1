63dcabe9f276877f6989af04ab57c5364a2bee2a
hkaithan <110191292+hkaithan@users.noreply.github.com>
2022-07-28 23:16:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert ("Hi there!");
+confirm("Do you want to view the content of this page?");
+var user = prompt("Please enter your name");
+alert("Welcome " + user +"! You are about to view the content of this page.");
+document.write("Yes " + user + ", " + "you are now a JavaScript coder! And you are viewing this text with the help of JavaScript.");
